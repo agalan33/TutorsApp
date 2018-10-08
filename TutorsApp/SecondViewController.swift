687ef5e8520d2c2bb227cb0ca5456ec31016e554
Adahid Galan <adahidgalan@adahids-macbook-pro.local>
2018-10-08 19:39:11 -0400

    Initial Commit

---
+++ TutorsApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TutorsApp
+//
+//  Created by Adahid Galan on 10/8/18.
+//  Copyright © 2018 UniversityHub. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
